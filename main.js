d25011b3e0cde59c4834d4821032816a4aee36c6
Nijaz Andelić <77886732+AndNijaz@users.noreply.github.com>
2024-10-31 16:40:45 +0100

    initial commit

---
+++ main.js
@@ -0,0 +1,14 @@
+const app = Vue.createApp({
+  data() {
+    return {
+      counter: 0,
+    };
+  },
+  methods: {
+    incrementCounter() {
+      this.counter++;
+    },
+  },
+});
+
+app.mount(document.getElementById("app"));
