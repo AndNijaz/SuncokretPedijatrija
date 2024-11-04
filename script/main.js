168041d1387b6015ba1c2075de4417429c36240c
Nikola Obradovic <nikola.obradovic@stu.ssst.edu.ba>
2024-11-05 00:08:38 +0100

    hero page almost complete

--- script/main.js
+++ script/main.js
@@ -12,3 +12,12 @@ const app = Vue.createApp({
 });
 
 app.mount(document.getElementById("appz"));
+
+//header
+const header = document.querySelector(".header");
+
+const btnToggleNav = document.querySelector(".btn-mobile-nav");
+
+btnToggleNav.addEventListener("click", () =>
+  header.classList.toggle("nav-open")
+);
