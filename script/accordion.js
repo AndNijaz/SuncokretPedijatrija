22ca1b2ed33da7605c39f0a393ac6e064f31430b
Nijaz Andelić <77886732+AndNijaz@users.noreply.github.com>
2024-11-06 23:26:03 +0100

    pricing done

--- script/accordion.js
+++ script/accordion.js
@@ -1,24 +1,33 @@
 document.querySelectorAll(".accordion-header").forEach((header) => {
   header.addEventListener("click", () => {
     const content = header.nextElementSibling;
-    const icon = header.querySelector(".accordion-icon");
-    const isOpen = content.style.maxHeight;
+    const downIcon = header.querySelector(".accordion-icon-down");
+    const upIcon = header.querySelector(".accordion-icon-up");
 
+    // Check if the current accordion item is open
+    const isOpen = content.style.maxHeight && content.style.maxHeight !== "0px";
+
+    // Close all other accordion items
     document.querySelectorAll(".accordion-content").forEach((item) => {
       item.style.maxHeight = null;
       item.style.paddingTop = "0";
       item.style.paddingBottom = "0";
+      const parentHeader = item.previousElementSibling;
+      parentHeader
+        .querySelector(".accordion-icon-down")
+        .classList.remove("icon-hidden");
+      parentHeader
+        .querySelector(".accordion-icon-up")
+        .classList.add("icon-hidden");
     });
 
-    document.querySelectorAll(".accordion-icon").forEach((icon) => {
-      icon.classList.remove("rotate");
-    });
-
+    // If the clicked item was previously closed, open it
     if (!isOpen) {
       content.style.maxHeight = content.scrollHeight + "px";
       content.style.paddingTop = "15px";
       content.style.paddingBottom = "15px";
-      icon.classList.add("rotate");
+      downIcon.classList.add("icon-hidden");
+      upIcon.classList.remove("icon-hidden");
     }
   });
 });
