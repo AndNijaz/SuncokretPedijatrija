74b3ecdfddf078ccb69d8c2c8e1e7ecfd3b605a7
Nikola Obradovic <nikola.obradovic@stu.ssst.edu.ba>
2024-11-10 00:05:10 +0100

    team page complete

--- script/toggle-modal.js
+++ script/toggle-modal.js
@@ -1,4 +1,13 @@
-function toggleText() {
-  const textContainer = document.querySelector(".toggle-text");
+function toggleText(n) {
+  let textContainer = "";
+  if (n === 1) {
+    textContainer = document.getElementById("toggle-1");
+  } else if (n === 2) {
+    textContainer = document.getElementById("toggle-2");
+  } else if (n === 3) {
+    textContainer = document.getElementById("toggle-3");
+  } else if (n === 4) {
+    textContainer = document.getElementById("toggle-4");
+  }
   textContainer.classList.toggle("active");
 }
