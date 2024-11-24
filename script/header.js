08d47ba396e238fb01d896cc45339b457b1eebe6
Nijaz Andelić <77886732+AndNijaz@users.noreply.github.com>
2024-11-24 22:33:05 +0100

    commit

--- script/header.js
+++ script/header.js
@@ -1,7 +1,1 @@
-const header = document.querySelector(".header");
-
-const btnToggleNav = document.querySelector(".btn-mobile-nav");
-
-btnToggleNav.addEventListener("click", () =>
-  header.classList.toggle("nav-open")
-);
+const header=document.querySelector(".header"),btnToggleNav=document.querySelector(".btn-mobile-nav");btnToggleNav.addEventListener("click",()=>header.classList.toggle("nav-open"));
