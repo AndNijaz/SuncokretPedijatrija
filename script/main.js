eed761da6d6995508e1f5465f02ade942832eabb
Nikola Obradovic <nikola.obradovic@stu.ssst.edu.ba>
2024-11-25 17:37:49 +0100

    map fix

--- script/main.js
+++ script/main.js
@@ -1,10 +1,10 @@
-var map = L.map("map").setView([43.866, 18.398], 14);
+var map = L.map("map").setView([43.8665, 18.4011], 16);
 L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
   maxZoom: 19,
   attribution:
     '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
 }).addTo(map);
-var marker = L.marker([43.866, 18.398]).addTo(map);
+var marker = L.marker([43.8665, 18.4011]).addTo(map);
 document.addEventListener("DOMContentLoaded", () => {
   let e = document.querySelectorAll(".main-nav-link"),
     t = window.location.pathname;
